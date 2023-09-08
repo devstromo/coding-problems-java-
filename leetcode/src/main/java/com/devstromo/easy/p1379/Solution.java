daa1988dfb544a44a3f2973e3b2a7adfcdccf711
alemoles <alemolerio@gmail.com>
2023-09-07 22:09:28 -0300

    Leetcode | Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ leetcode/src/main/java/com/devstromo/easy/p1379/Solution.java
@@ -0,0 +1,17 @@
+package com.devstromo.easy.p1379;
+
+class Solution {
+    public static TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) {
+            return null;
+        }
+        if (original == target) {
+            return cloned;
+        }
+        var left = getTargetCopy(original.left, cloned.left, target);
+        if (left != null) {
+            return left;
+        }
+        return getTargetCopy(original.right, cloned.right, target);
+    }
+}
---
+++ leetcode/src/main/java/com/devstromo/easy/p1379/TreeNode.java
@@ -0,0 +1,11 @@
+package com.devstromo.easy.p1379;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
