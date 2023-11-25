5c5de82e53c42e3a6d63f41d7523100f3697583e
alemoles <alemolerio@gmail.com>
2023-11-24 23:50:21 -0300

    Leetcode | Number of 1 Bits | Kotlin

---
+++ leetcode/src/main/kotlin/com/devstromo/easy/p191/SolutionKt.kt
@@ -0,0 +1,13 @@
+package com.devstromo.easy.p191
+
+class SolutionKt {
+    fun hammingWeight(n: Int): Int {
+        var bits = n
+        var count = 0
+        while (bits != 0) {
+            count += bits and 1
+            bits = bits ushr 1 // Unsigned right shift
+        }
+        return count
+    }
+}
---
+++ leetcode/src/test/kotlin/com/devstromo/easy/p191/SolutionKtUnitTest.kt
@@ -0,0 +1,20 @@
+package com.devstromo.easy.p191
+
+import com.devstromo.easy.p191.SolutionKt
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionKtUnitTest {
+
+    private val solutionKt = SolutionKt()
+
+    @Test
+    fun testHammingWeightKt() {
+        assertEquals(3, solutionKt.hammingWeight(11))
+    }
+
+    @Test
+    fun testHammingWeightKt2() {
+        assertEquals(1, solutionKt.hammingWeight(128))
+    }
+}
