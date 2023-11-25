9e4a2485a526c2e48b10619a561d65864bd12740
alemoles <alemolerio@gmail.com>
2023-11-24 23:55:23 -0300

    Leetcode | Number of 1 Bits | Kotlin

--- leetcode/src/main/kotlin/com/devstromo/easy/p191/SolutionKt.kt
+++ leetcode/src/main/kotlin/com/devstromo/easy/p191/SolutionKt.kt
@@ -10,4 +10,14 @@ class SolutionKt {
         }
         return count
     }
+
+    fun hammingWeightBest(n: Int): Int {
+        var count = 0
+        var num = n
+        while (num != 0) {
+            num = num and (num - 1)
+            count++
+        }
+        return count
+    }
 }
