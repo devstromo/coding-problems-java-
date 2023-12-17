a7293650e7d34590bde167ba552b4fcf0209bae6
alemoles <alemolerio@gmail.com>
2023-12-16 21:12:21 -0300

    Leetcode | Daily Temperatures | Java

--- leetcode/src/main/java/com/devstromo/medium/p739/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p739/Solution.java
@@ -1,6 +1,7 @@
 package com.devstromo.medium.p739;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
@@ -16,5 +17,19 @@ public class Solution {
         }
 
         return answer;
+    }
+
+    public int[] dailyTemperaturesBest(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int day = stack.pop();
+                res[day] = i - day;
+            }
+            stack.push(i);
+        }
+        res[temperatures.length - 1] = 0;
+        return res;
     }
 }
--- leetcode/src/test/java/com/devstromo/medium/p739/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p739/SolutionUnitTest.java
@@ -36,4 +36,32 @@ class SolutionUnitTest {
         assertArrayEquals(expected, solution.dailyTemperatures(input));
     }
 
+    @Test
+    public void testDailyTemperaturesBest() {
+        var input = new int[]{58, 56, 61, 55, 57, 44, 40, 35, 46};
+        var expected = new int[]{2, 1, 0, 1, 0, 3, 2, 1, 0};
+        assertArrayEquals(expected, solution.dailyTemperaturesBest(input));
+    }
+
+    @Test
+    public void testDailyTemperaturesBest2() {
+        var input = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        var expected = new int[]{1, 1, 4, 2, 1, 1, 0, 0};
+        assertArrayEquals(expected, solution.dailyTemperaturesBest(input));
+    }
+
+    @Test
+    public void testDailyTemperaturesBest3() {
+        var input = new int[]{30, 40, 50, 60};
+        var expected = new int[]{1, 1, 1, 0};
+        assertArrayEquals(expected, solution.dailyTemperaturesBest(input));
+    }
+
+    @Test
+    public void testDailyTemperaturesBest4() {
+        var input = new int[]{30, 60, 90};
+        var expected = new int[]{1, 1, 0};
+        assertArrayEquals(expected, solution.dailyTemperaturesBest(input));
+    }
+
 }
