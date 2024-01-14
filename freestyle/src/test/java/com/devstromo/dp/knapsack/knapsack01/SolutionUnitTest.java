2c15e53b4d1b742400810b2aaae8f435b7364e9a
alemoles <alemolerio@gmail.com>
2024-01-14 14:23:17 -0300

    Freestyle | 0/1 Knapsack | Java

--- freestyle/src/test/java/com/devstromo/dp/knapsack/knapsack01/SolutionUnitTest.java
+++ freestyle/src/test/java/com/devstromo/dp/knapsack/knapsack01/SolutionUnitTest.java
@@ -65,4 +65,26 @@ class SolutionUnitTest {
         var result = solution.knapsack01Selected(n, w, weights, profits);
         assertArrayEquals(expectedResult, result);
     }
+
+    @Test
+    void testKnapsack01With1DArraySelectedItems() {
+        int n = 3;
+        int w = 50;
+        var weights = new int[]{10, 20, 30};
+        var profits = new int[]{60, 100, 120};
+        var expectedResult = new int[]{0, 1, 1};
+        var result = solution.knapsack01With1DArraySelected(n, w, weights, profits);
+        assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    void testKnapsack01With1DArraySelectedItems2() {
+        int n = 4;
+        int w = 5;
+        var weights = new int[]{3, 1, 2, 1};
+        var profits = new int[]{2000, 1000, 1500, 500};
+        var expectedResult = new int[]{1, 0, 1, 0};
+        var result = solution.knapsack01With1DArraySelected(n, w, weights, profits);
+        assertArrayEquals(expectedResult, result);
+    }
 }
