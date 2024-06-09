67aeb356237a344d97df9b645a51544343218e57
alemoles <alemolerio@gmail.com>
2024-06-09 00:05:05 -0300

    Freestyle | Subset Sum Problem | Java

--- freestyle/src/main/java/com/devstromo/dp/subset_sum/SubsetSum.java
+++ freestyle/src/main/java/com/devstromo/dp/subset_sum/SubsetSum.java
@@ -60,20 +60,31 @@ public class SubsetSum {
             for (int j = 0; j <= sum; j++) {
                 if (j == 0) {
                     dp[i % 2][j] = true;
-                }
-                else if (i == 0) {
+                } else if (i == 0) {
                     dp[i % 2][j] = false;
-                }
-                else if (numbers[i - 1] <= j) {
+                } else if (numbers[i - 1] <= j) {
                     dp[i % 2][j] = dp[(i + 1) % 2]
                             [j - numbers[i - 1]] || dp[(i + 1) % 2][j];
-                }
-                else {
+                } else {
                     dp[i % 2][j] = dp[(i + 1) % 2][j];
                 }
             }
         }
         return dp[n % 2][sum];
+    }
+
+    public boolean tabular1DOptimized(int[] numbers, int sum) {
+        var n = numbers.length;
+        var dp = new int[sum + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= numbers[i]; j--) {
+                if (dp[j - numbers[i]] == 1)
+                    dp[j] = 1;
+            }
+        }
+        return dp[sum] == 1;
     }
 
     private boolean isSubsetSumRecursive(int[] numbers, int n, int sum) {
