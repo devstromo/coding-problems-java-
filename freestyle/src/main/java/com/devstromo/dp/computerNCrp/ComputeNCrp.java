78b9c588c0ac26664fcef72a8db04958b2587830
alemoles <alemolerio@gmail.com>
2024-06-13 21:52:31 -0300

    Freestyle | Compute nCr % p | Java

--- freestyle/src/main/java/com/devstromo/dp/computerNCrp/ComputeNCrp.java
+++ freestyle/src/main/java/com/devstromo/dp/computerNCrp/ComputeNCrp.java
@@ -25,4 +25,18 @@ public class ComputeNCrp {
         }
         return C[r];
     }
+
+    public int solution2DArray(int n, int r, int p) {
+        var dp = new int[n + 1][r + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, r); j++) {
+                if (j == 0 || j == i) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % p;
+                }
+            }
+        }
+        return dp[n][r];
+    }
 }
--- freestyle/src/test/java/com/devstromo/dp/computerNCrp/ComputeNCrpUnitTest.java
+++ freestyle/src/test/java/com/devstromo/dp/computerNCrp/ComputeNCrpUnitTest.java
@@ -16,8 +16,15 @@ class ComputeNCrpUnitTest {
 
     @Test
     public void testComputeNCrpRecursive() {
-        assertEquals(1, solution.solution(1, 1, 1));
+        assertEquals(1, solution.solutionRecursive(1, 1, 1));
         assertEquals(3, solution.solutionRecursive(8, 2, 5));
         assertEquals(10, solution.solutionRecursive(5, 2, 13));
+    }
+
+    @Test
+    public void testComputeNCrp2DArray() {
+        assertEquals(1, solution.solution2DArray(1, 1, 1));
+        assertEquals(3, solution.solution2DArray(8, 2, 5));
+        assertEquals(10, solution.solution2DArray(5, 2, 13));
     }
 }
