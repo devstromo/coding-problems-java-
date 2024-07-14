da424932522b7687b2ea9a5034427ce5fb598236
alemoles <alemolerio@gmail.com>
2024-07-14 17:33:31 -0300

    Freestyle | Coin Change | Java

---
+++ freestyle/src/main/java/com/devstromo/dp/coin_change/CoinChange.java
@@ -0,0 +1,30 @@
+package com.devstromo.dp.coin_change;
+
+public class CoinChange {
+    public int solution(int[] coins, int amount) {
+        final var n = coins.length;
+        final var dp = new int[n + 1][amount + 1];
+        var i = 0;
+        var j = 1;
+        for (i = 0; i <= amount; i++) {
+            dp[0][i] = Integer.MAX_VALUE - 1;
+        }
+        for (i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= amount; j++) {
+                if (coins[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                }
+            }
+        }
+        return dp[coins.length][amount] >= Integer.MAX_VALUE - 1 ? -1 : dp[coins.length][amount];
+    }
+
+    private int min(int a, int b) {
+        return a > b ? b : a;
+    }
+}
---
+++ freestyle/src/test/java/com/devstromo/dp/coin_change/CoinChangeUnitTest.java
@@ -0,0 +1,51 @@
+package com.devstromo.dp.coin_change;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoinChangeUnitTest {
+    private final CoinChange coinChange = new CoinChange();
+
+    @Test
+    public void testCoinChangeBasic() {
+        final var coins = new int[]{1, 2, 5};
+
+        assertEquals(3, coinChange.solution(coins, 11));
+        assertEquals(2, coinChange.solution(coins, 3));
+    }
+
+    @Test
+    public void testCoinChangeEdgeCases() {
+        final var coins1 = new int[]{1};
+        final var coins2 = new int[]{2};
+
+        assertEquals(0, coinChange.solution(coins1, 0));
+        assertEquals(1, coinChange.solution(coins1, 1));
+        assertEquals(-1, coinChange.solution(coins2, 3));
+    }
+
+    @Test
+    public void testCoinChangeLargeInputs() {
+        final var coins1 = new int[]{1, 2, 5, 10, 20, 50};
+        final var coins2 = new int[]{1, 5, 10, 25, 50};
+
+        assertEquals(2, coinChange.solution(coins1, 100));
+        assertEquals(8, coinChange.solution(coins2, 99));
+    }
+
+    @Test
+    public void testCoinChangeImpossibleCases() {
+        final var coins = new int[]{2, 5, 10};
+
+        assertEquals(-1, coinChange.solution(coins, 3));
+    }
+
+    @Test
+    public void testCoinChangeAllCoinsLargerThanAmount() {
+        final var coins = new int[]{5, 10, 25};
+
+        assertEquals(-1, coinChange.solution(coins, 3));
+    }
+
+}
