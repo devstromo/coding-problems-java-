dc40392792a3c15c64e67e7dfc3bbc77ef75c703
alemoles <alemolerio@gmail.com>
2024-08-30 22:58:15 -0300

    Leetcode | Check if Numbers Are Ascending in a Sentence | Java

--- leetcode/src/main/java/com/devstromo/easy/p2042/Solution.java
+++ leetcode/src/main/java/com/devstromo/easy/p2042/Solution.java
@@ -20,6 +20,35 @@ public class Solution {
         return true;
     }
 
+
+    public boolean areNumbersAscendingBest(String s) {
+        int prevNumber = -1;
+        int currNumber = 0;
+        boolean isNumber = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                currNumber = currNumber * 10 + (c - '0');
+                isNumber = true;
+            } else if (isNumber) {
+                if (currNumber <= prevNumber) {
+                    return false;
+                }
+                prevNumber = currNumber;
+                currNumber = 0;
+                isNumber = false;
+            }
+        }
+
+        if (isNumber && currNumber <= prevNumber) {
+            return false;
+        }
+
+        return true;
+    }
+
     private boolean isNumber(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (!Character.isDigit(s.charAt(i)))
--- leetcode/src/test/java/com/devstromo/easy/p2042/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/easy/p2042/SolutionUnitTest.java
@@ -14,4 +14,11 @@ class SolutionUnitTest {
         assertFalse(solution.areNumbersAscending("sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s"));
     }
 
+    @Test
+    void testIfNumbersAreAscendingBest() {
+        assertTrue(solution.areNumbersAscendingBest("1 box has 3 blue 4 red 6 green and 12 yellow marbles"));
+        assertFalse(solution.areNumbersAscendingBest("hello world 5 x 5"));
+        assertFalse(solution.areNumbersAscendingBest("sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s"));
+    }
+
 }
