d3e8fbc8593173114288aa0c1997baba32d3c393
alemoles <alemolerio@gmail.com>
2024-09-12 22:43:50 -0300

    Leetcode | Coin Change | Java

--- leetcode/src/main/java/com/devstromo/medium/p322/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p322/Solution.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Solution {
 
     public int coinChange(int[] coins, int amount) {
-        var dp = new int[amount + 1];
+        final var dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE - 1);
         dp[0] = 0;
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = coins[i]; j <= amount; j++) {
-                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
-                    dp[j] = Math.min(dp[j], 1 + dp[j - coins[i]]);
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                if (dp[j - coin] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], 1 + dp[j - coin]);
                 }
             }
         }
