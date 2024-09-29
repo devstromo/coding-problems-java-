802e9fb2a5d821e1230f7e5b7689c6a6170b5113
alemoles <alemolerio@gmail.com>
2024-09-29 16:01:41 -0300

    Leetcode | Minimum Path Sum | Java

--- leetcode/src/main/java/com/devstromo/medium/p64/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p64/Solution.java
@@ -39,4 +39,23 @@ public class Solution {
         return grid[m - 1][n - 1];
     }
 
+    public int minPathSumBestSolution(int[][] grid) {
+        int[][] dp = new int[grid.length + 1][grid[0].length + 1];
+        return sum(grid, 0, 0, dp);
+    }
+
+    public int sum(int[][] arr, int i, int j, int[][] dp) {
+        if (i >= arr.length || j >= arr[0].length) {
+            return Integer.MAX_VALUE;
+        }
+        if (i == arr.length - 1 && j == arr[0].length - 1) {
+            return arr[i][j];
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+
+        return dp[i][j] = arr[i][j] + Math.min(sum(arr, i, j + 1, dp), sum(arr, i + 1, j, dp));
+    }
+
 }
--- leetcode/src/test/java/com/devstromo/medium/p64/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p64/SolutionUnitTest.java
@@ -40,4 +40,20 @@ class SolutionUnitTest {
         );
     }
 
+    @Test
+    void testMaximumNumPathBestSolution() {
+        assertEquals(7, solution.minPathSumBestSolution(new int[][]{
+                        {1, 3, 1},
+                        {1, 5, 1},
+                        {4, 2, 1}
+                })
+        );
+
+        assertEquals(12, solution.minPathSumBestSolution(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6}
+                })
+        );
+    }
+
 }
