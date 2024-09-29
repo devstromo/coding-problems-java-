f3a87a3b264d7f5cc1db6a2536bd2bca9b7562fe
alemoles <alemolerio@gmail.com>
2024-09-29 15:55:17 -0300

    Leetcode | Minimum Path Sum | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p64/SolutionUnitTest.java
@@ -0,0 +1,27 @@
+package com.devstromo.medium.p64;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void testMaximumNumPath() {
+        assertEquals(7, solution.minPathSum(new int[][]{
+                        {1, 3, 1},
+                        {1, 5, 1},
+                        {4, 2, 1}
+                })
+        );
+
+        assertEquals(12, solution.minPathSum(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6}
+                })
+        );
+    }
+
+}
