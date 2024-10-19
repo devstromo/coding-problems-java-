454e71dc24715f41cf1ca0ad598b36765b3c9eca
alemoles <alemolerio@gmail.com>
2024-10-19 16:22:34 -0300

    Leetcode | Linked List Cycle | Java

---
+++ leetcode/src/main/java/com/devstromo/easy/p141/Solution.java
@@ -0,0 +1,26 @@
+package com.devstromo.easy.p141;
+
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        var slow = head;
+        var fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
