edd1c9ede3c4a379307e395298c831709ff96b96
alemoles <alemolerio@gmail.com>
2024-10-29 20:36:50 -0300

    Leetcode | Next Greater Element I | Java

--- leetcode/src/main/java/com/devstromo/easy/p496/Solution.java
+++ leetcode/src/main/java/com/devstromo/easy/p496/Solution.java
@@ -16,4 +16,27 @@ public class Solution {
             nums1[i] = map.getOrDefault(nums1[i], -1);
         return nums1;
     }
+
+    public int[] nextGreaterElementBestVariant(int[] nums1, int[] nums2) {
+        var vocab = new HashMap<Integer, Integer>();
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums2.length; i++) vocab.put(nums2[i], i);
+        for (int i = 0; i < nums1.length; i++) result[i] = greater(vocab.get(nums1[i]), nums2);
+        return result;
+    }
+
+    public int[] nextGreaterElementBest(int[] nums1, int[] nums2) {
+        int[] vocab = new int[10001];
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums2.length; i++) vocab[nums2[i]] = i;
+        for (int i = 0; i < nums1.length; i++) result[i] = greater(vocab[nums1[i]], nums2);
+        return result;
+    }
+
+    public int greater(int idx, int[] nums2) {
+        for (int j = idx + 1; j < nums2.length; j++) {
+            if (nums2[j] > nums2[idx]) return nums2[j];
+        }
+        return -1;
+    }
 }
--- leetcode/src/test/java/com/devstromo/easy/p496/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/easy/p496/SolutionUnitTest.java
@@ -12,4 +12,16 @@ class SolutionUnitTest {
         assertArrayEquals(new int[]{-1, 3, -1}, solution.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}));
         assertArrayEquals(new int[]{3, -1}, solution.nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4}));
     }
+
+    @Test
+    void testNextGreaterElementBest() {
+        assertArrayEquals(new int[]{-1, 3, -1}, solution.nextGreaterElementBest(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}));
+        assertArrayEquals(new int[]{3, -1}, solution.nextGreaterElementBest(new int[]{2, 4}, new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    void testNextGreaterElementBestVariant() {
+        assertArrayEquals(new int[]{-1, 3, -1}, solution.nextGreaterElementBestVariant(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}));
+        assertArrayEquals(new int[]{3, -1}, solution.nextGreaterElementBestVariant(new int[]{2, 4}, new int[]{1, 2, 3, 4}));
+    }
 }
