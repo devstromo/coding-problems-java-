de6ab378362399a19dd6e2cc5b50ba2583b4521c
alemoles <alemolerio@gmail.com>
2024-10-31 22:13:38 -0300

    Leetcode | Coin Change II | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p518/SolutionUnitTest.java
@@ -0,0 +1,17 @@
+package com.devstromo.medium.p518;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void testChangeCoinII() {
+        assertEquals(4, solution.change(5, new int[]{1, 2, 5}));
+        assertEquals(0, solution.change(3, new int[]{2}));
+        assertEquals(1, solution.change(10, new int[]{10}));
+    }
+
+}
