8f46476eeddaa435d16819b1321d6b0414c51dd1
alemoles <alemolerio@gmail.com>
2024-11-24 17:16:46 -0300

    Leetcode | Length of Longest Fibonacci Subsequence | Java

--- leetcode/src/main/java/com/devstromo/medium/p873/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p873/Solution.java
@@ -1,6 +1,7 @@
 package com.devstromo.medium.p873;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class Solution {
@@ -28,5 +29,32 @@ public class Solution {
         }
 
         return maxLen >= 3 ? maxLen : 0;
+    }
+
+    public int lenLongestFibSubseqBest(int[] arr) {
+        final var index = new HashSet<>();
+        for (int n : arr) {
+            index.add(n);
+        }
+        int max = 2;
+        for (int i = 0; i < arr.length - max; i++) {
+            if (arr[i] * Math.pow(1.618, max - 1) > arr[arr.length - 1])
+                break;
+            for (int j = i + 1; j < arr.length - max + 1; j++) {
+                if (arr[j] * Math.pow(1.618, max - 2) > arr[arr.length - 1])
+                    break;
+                int n2 = arr[i];
+                int n1 = arr[j];
+                int len = 2;
+                while (index.contains(n1 + n2)) {
+                    n1 = n1 + n2;
+                    n2 = n1 - n2;
+                    len++;
+                }
+                if (len > max)
+                    max = len;
+            }
+        }
+        return max < 3 ? 0 : max;
     }
 }
--- leetcode/src/test/java/com/devstromo/medium/p873/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p873/SolutionUnitTest.java
@@ -14,4 +14,10 @@ class SolutionUnitTest {
         assertEquals(3, solution.lenLongestFibSubseq(new int[]{1, 3, 7, 11, 12, 14, 18}));
     }
 
+    @Test
+    public void testLenLongestFibSubseqBest() {
+        assertEquals(5, solution.lenLongestFibSubseqBest(new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
+        assertEquals(3, solution.lenLongestFibSubseqBest(new int[]{1, 3, 7, 11, 12, 14, 18}));
+    }
+
 }
