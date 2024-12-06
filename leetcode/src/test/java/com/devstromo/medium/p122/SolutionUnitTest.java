d344cab4176620351533eaef584b6189e45b6e98
alemoles <alemolerio@gmail.com>
2024-12-05 22:51:48 -0300

    Leetcode | Best Time to Buy and Sell Stock II | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p122/SolutionUnitTest.java
@@ -0,0 +1,17 @@
+package com.devstromo.medium.p122;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void testMaxProfitII() {
+        assertEquals(7, solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        assertEquals(4, solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(0, solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+
+}
