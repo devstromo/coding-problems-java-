653ddf4f5da513e1ee146ff36af77381fbaebb59
alemoles <alemolerio@gmail.com>
2024-12-13 23:26:21 -0300

    Leetcode | Find the Middle Index in Array | Java

--- leetcode/src/main/java/com/devstromo/easy/p1991/Solution.java
+++ leetcode/src/main/java/com/devstromo/easy/p1991/Solution.java
@@ -20,4 +20,22 @@ public class Solution {
         }
         return -1;
     }
+
+    public int findMiddleIndexLinear(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
 }
--- leetcode/src/test/java/com/devstromo/easy/p1991/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/easy/p1991/SolutionUnitTest.java
@@ -11,8 +11,15 @@ class SolutionUnitTest {
     @Test
     void testFindMiddleIndex() {
         assertEquals(3, solution.findMiddleIndex(new int[]{2, 3, -1, 8, 4}));
-//        assertEquals(2, solution.findMiddleIndex(new int[]{1, -1, 4}));
-//        assertEquals(-1, solution.findMiddleIndex(new int[]{2, 5}));
+        assertEquals(2, solution.findMiddleIndex(new int[]{1, -1, 4}));
+        assertEquals(-1, solution.findMiddleIndex(new int[]{2, 5}));
+    }
+
+    @Test
+    void testFindMiddleIndexBest() {
+        assertEquals(3, solution.findMiddleIndexLinear(new int[]{2, 3, -1, 8, 4}));
+        assertEquals(2, solution.findMiddleIndexLinear(new int[]{1, -1, 4}));
+        assertEquals(-1, solution.findMiddleIndexLinear(new int[]{2, 5}));
     }
 
 }
