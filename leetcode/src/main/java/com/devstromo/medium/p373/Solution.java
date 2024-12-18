33d6270e57cd6eb962e1f8fb8ea51673c5f615d4
alemoles <alemolerio@gmail.com>
2024-12-17 21:53:34 -0300

    Leetcode | Find K Pairs with Smallest Sums | Java

--- leetcode/src/main/java/com/devstromo/medium/p373/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p373/Solution.java
@@ -1,10 +1,7 @@
 package com.devstromo.medium.p373;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
@@ -26,6 +23,103 @@ public class Solution {
             k--;
         }
         return result;
+    }
+
+    public List<List<Integer>> kSmallestPairsBest(int[] nums1, int[] nums2, int k) {
+        return new AbstractList<List<Integer>>() {
+
+            private List<List<Integer>> pairs;
+
+            @Override
+            public List<Integer> get(int index) {
+                init();
+                return pairs.get(index);
+            }
+
+            @Override
+            public int size() {
+                init();
+                return pairs.size();
+            }
+
+            private void load() {
+                int n = nums1.length, m = nums2.length;
+                int left = nums1[0] + nums2[0];
+                int right = nums1[n - 1] + nums2[m - 1];
+                int middle;
+
+                while (left <= right) {
+
+                    middle = (left + right) / 2;
+
+                    long count = getCount(nums1, nums2, middle, k);
+
+                    if (count < k) {
+                        left = middle + 1;
+                    } else if (count > k) {
+                        right = middle - 1;
+                    } else {
+                        left = middle;
+                        break;
+                    }
+                }
+                getPairs(nums1, nums2, left, k);
+            }
+
+            private long getCount(int[] nums1, int[] nums2, int goal, int k) {
+                int prevRight = nums2.length - 1, count = 0;
+
+                for (int i = 0; i < nums1.length && nums1[i] + nums2[0] <= goal; i++) {
+                    int left = 0, right = prevRight, position = -1;
+
+                    while (left <= right) {
+                        int middle = (right + left) / 2;
+                        int sum = nums1[i] + nums2[middle];
+
+                        if (sum <= goal) {
+                            position = middle;
+                            left = middle + 1;
+                        } else {
+                            right = middle - 1;
+                        }
+                    }
+                    if (position >= 0) {
+                        count += position + 1;
+                        prevRight = position;
+                    }
+                    if (count > k) {
+                        return count;
+                    }
+                }
+                return count;
+            }
+
+            private List<List<Integer>> getPairs(int[] nums1, int[] nums2, int sum, int k) {
+                pairs = new ArrayList<>();
+
+                for (int i = 0; i < nums1.length; i++) {
+                    for (int j = 0; j < nums2.length && nums1[i] + nums2[j] < sum; j++) {
+                        pairs.add(Arrays.asList(nums1[i], nums2[j]));
+                    }
+                }
+
+                for (int i = 0; i < nums1.length; i++) {
+                    for (int j = 0; j < nums2.length && nums1[i] + nums2[j] <= sum && pairs.size() < k; j++) {
+                        if (nums1[i] + nums2[j] == sum) {
+                            pairs.add(Arrays.asList(nums1[i], nums2[j]));
+                        }
+                    }
+                }
+                return pairs;
+            }
+
+            public void init() {
+                if (null == pairs) {
+                    load();
+                    System.gc();
+                }
+            }
+        };
     }
 
     record Pair(int first, int second) {
--- leetcode/src/test/java/com/devstromo/medium/p373/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p373/SolutionUnitTest.java
@@ -10,9 +10,15 @@ class SolutionUnitTest {
     private final Solution solution = new Solution();
 
     @Test
-    void solve() {
+    void testKSmallestPairs() {
         assertEquals(List.of(List.of(1, 2), List.of(1, 4), List.of(1, 6)), solution.kSmallestPairs(new int[]{1, 7, 11}, new int[]{2, 4, 6}, 3));
         assertEquals(List.of(List.of(1, 1), List.of(1, 1)), solution.kSmallestPairs(new int[]{1, 1, 2}, new int[]{1, 2, 3}, 2));
+    }
+
+    @Test
+    void testKSmallestPairsBest() {
+        assertEquals(List.of(List.of(1, 2), List.of(1, 4), List.of(1, 6)), solution.kSmallestPairsBest(new int[]{1, 7, 11}, new int[]{2, 4, 6}, 3));
+        assertEquals(List.of(List.of(1, 1), List.of(1, 1)), solution.kSmallestPairsBest(new int[]{1, 1, 2}, new int[]{1, 2, 3}, 2));
 
     }
 }
