a2c9aa237978ccf8a695fe12442a562fae91a12e
alemoles <alemolerio@gmail.com>
2024-12-20 23:46:43 -0300

    Leetcode | Merge Intervals | Java

--- leetcode/src/main/java/com/devstromo/medium/p56/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p56/Solution.java
@@ -21,4 +21,44 @@ public class Solution {
 
         return Arrays.copyOf(merged, index);
     }
+
+    public int[][] mergeBest(int[][] intervals) {
+        // Sweepline algorithm
+        int max = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            max = Math.max(intervals[i][0], max);
+        }
+        int[] mp = new int[max + 1];
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            mp[start] = Math.max(end + 1, mp[start]);
+        }
+        int r = 0;
+        int initialStart = -1;
+        int have = -1;
+        for (int i = 0; i < mp.length; i++) {
+            if (mp[i] != 0) {
+                if (initialStart == -1)
+                    initialStart = i;
+                have = Math.max(mp[i] - 1, have);
+            }
+            if (have == i) {
+                intervals[r++] = new int[] { initialStart, have };
+                initialStart = -1;
+                have = -1;
+            }
+        }
+        if (initialStart != -1) {
+            intervals[r++] = new int[] { initialStart, have };
+        }
+        if (intervals.length == r) {
+            return intervals;
+        }
+        int[][] res = new int[r][];
+        for (int i = 0; i < r; i++) {
+            res[i] = intervals[i];
+        }
+        return res;
+    }
 }
--- leetcode/src/test/java/com/devstromo/medium/p56/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p56/SolutionUnitTest.java
@@ -17,4 +17,14 @@ class SolutionUnitTest {
         }, solution.merge(new int[][]{{1, 4}, {4, 5}}));
     }
 
+    @Test
+    void testMergeBest() {
+        assertArrayEquals(new int[][]{
+                {1, 6}, {8, 10}, {15, 18}
+        }, solution.mergeBest(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}}));
+        assertArrayEquals(new int[][]{
+                {1, 5}
+        }, solution.mergeBest(new int[][]{{1, 4}, {4, 5}}));
+    }
+
 }
