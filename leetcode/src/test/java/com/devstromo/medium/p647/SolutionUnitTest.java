17c598d901f7b7a1c4ba49f78bb1865963843dc7
alemoles <alemolerio@gmail.com>
2025-01-10 20:49:52 -0300

    Leetcode | Palindromic Substrings | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p647/SolutionUnitTest.java
@@ -0,0 +1,17 @@
+package com.devstromo.medium.p647;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Count Palindromic Substrings")
+    void testCountSubstrings() {
+        assertEquals(3, solution.countSubstrings("abc"));
+        assertEquals(6, solution.countSubstrings("aaa"));
+    }
+}
