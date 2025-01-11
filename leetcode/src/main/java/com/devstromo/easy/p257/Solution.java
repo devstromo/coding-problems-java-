7261f3e79f4867e80f383474dea990d1f4f61136
alemoles <alemolerio@gmail.com>
2025-01-11 19:39:02 -0300

    Leetcode | Binary Tree Paths | Java

--- leetcode/src/main/java/com/devstromo/easy/p257/Solution.java
+++ leetcode/src/main/java/com/devstromo/easy/p257/Solution.java
@@ -13,6 +13,27 @@ public class Solution {
         return paths;
     }
 
+    public List<String> binaryTreePathsBest(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        dfs(root, new StringBuilder(), ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, StringBuilder sb, List<String> ans) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            ans.add(sb.append(root.val).toString());
+            return;
+        }
+
+        final int length = sb.length();
+        dfs(root.left, sb.append(root.val).append("->"), ans);
+        sb.setLength(length);
+        dfs(root.right, sb.append(root.val).append("->"), ans);
+        sb.setLength(length);
+    }
+
     private void constructPaths(TreeNode node, String path, List<String> paths) {
         if (node != null) {
             path += node.val;
--- leetcode/src/test/java/com/devstromo/easy/p257/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/easy/p257/SolutionUnitTest.java
@@ -19,4 +19,13 @@ class SolutionUnitTest {
         assertEquals(List.of("1->2->5", "1->3"), solution.binaryTreePaths(root1));
     }
 
+    @Test
+    @DisplayName("Test Binary Tree Paths Best")
+    void testBinaryTreePathsBest() {
+        final var root1 = new Solution.TreeNode(1);
+        root1.left = new Solution.TreeNode(2, null, new Solution.TreeNode(5));
+        root1.right = new Solution.TreeNode(3);
+        assertEquals(List.of("1->2->5", "1->3"), solution.binaryTreePathsBest(root1));
+    }
+
 }
