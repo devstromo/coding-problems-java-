873852395c6109e58c27d78564e6dd80fc68370b
alemoles <alemolerio@gmail.com>
2025-01-12 13:23:20 -0300

    Leetcode | Minimum Insertion Steps to Make a String Palindrome | Java

---
+++ leetcode/src/test/java/com/devstromo/hard/p1312/SolutionUnitTest.java
@@ -0,0 +1,19 @@
+package com.devstromo.hard.p1312;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Minimum Insertion Steps to Make a String Palindrome")
+    void testMinInsertions() {
+        assertEquals(0, solution.minInsertions("zzazz"));
+        assertEquals(2, solution.minInsertions("mbadm"));
+        assertEquals(5, solution.minInsertions("leetcode"));
+    }
+
+}
