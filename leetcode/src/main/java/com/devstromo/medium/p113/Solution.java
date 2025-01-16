60c11c454a3b7835fd975e92223f94428f828b2d
alemoles <alemolerio@gmail.com>
2025-01-16 20:53:57 -0300

    Leetcode | Path Sum II | Java

--- leetcode/src/main/java/com/devstromo/medium/p113/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p113/Solution.java
@@ -12,6 +12,54 @@ class Solution {
         return result;
     }
 
+    public List<List<Integer>> pathSumBest(TreeNode root, int targetSum) {
+        return new java.util.AbstractList(){
+            private List<List<Integer>> result;
+
+            private void init(){
+                result = new ArrayList();
+                if(root == null){
+                    return;
+                }
+                backtrack(root, targetSum, new ArrayList(), result);
+            }
+
+            private void backtrack(TreeNode node, int target, List<Integer> items, List<List<Integer>> result){
+                if(node == null){
+                    return;
+                }
+                items.add(node.val);
+                if(node.left == null && node.right == null && target - node.val == 0){
+                    result.add(new ArrayList(items));
+                }
+                backtrack(node.left, target - node.val, items, result);
+                backtrack(node.right, target - node.val, items, result);
+                items.remove(items.size() - 1);
+            }
+
+            //5:    22 - 5 = 17
+            //4:    17 - 4 = 13
+            //11:   13 - 11 = 2
+            //7:    2  - 7 = -5 (out)
+            //2:    2 - 2  = 0 (add)
+            // remove -> 2
+
+
+            @Override
+            public int size(){
+                if(result == null){
+                    init();
+                }
+                return result.size();
+            }
+
+            @Override
+            public List<Integer> get(int index){
+                return result.get(index);
+            }
+        };
+    }
+
     private void dfs(TreeNode node, int targetSum, List<Integer> currentPath, List<List<Integer>> result) {
         if (node == null) {
             return;
--- leetcode/src/test/java/com/devstromo/medium/p113/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p113/SolutionUnitTest.java
@@ -37,7 +37,39 @@ class SolutionUnitTest {
                 )
         );
 
+        assertEquals(List.of(List.of(5, 4, 11, 2), List.of(5, 8, 4, 5)), solution.pathSum(root, 22));
         assertEquals(List.of(), solution.pathSum(root2, 5));
+    }
+
+    @Test
+    @DisplayName("Test Path Sum II in Binary Tree Best")
+    void testPathSumIIBest() {
+        Solution.TreeNode root = new Solution.TreeNode(5,
+                new Solution.TreeNode(4,
+                        new Solution.TreeNode(11,
+                                new Solution.TreeNode(7),
+                                new Solution.TreeNode(2)
+                        ),
+                        null
+                ),
+                new Solution.TreeNode(8,
+                        new Solution.TreeNode(13),
+                        new Solution.TreeNode(4,
+                                new Solution.TreeNode(5),
+                                new Solution.TreeNode(1)
+                        )
+                )
+        );
+
+        Solution.TreeNode root2 = new Solution.TreeNode(1,
+                new Solution.TreeNode(2
+                ),
+                new Solution.TreeNode(3
+                )
+        );
+
+        assertEquals(List.of(List.of(5, 4, 11, 2), List.of(5, 8, 4, 5)), solution.pathSumBest(root, 22));
+        assertEquals(List.of(), solution.pathSumBest(root2, 5));
     }
 
 }
