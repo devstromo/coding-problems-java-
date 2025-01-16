520dec27c7cc7bf55ccf5fc65730fba99028fa53
alemoles <alemolerio@gmail.com>
2025-01-16 20:47:42 -0300

    Leetcode | Path Sum | Java

--- leetcode/src/main/java/com/devstromo/easy/p112/Solution.java
+++ leetcode/src/main/java/com/devstromo/easy/p112/Solution.java
@@ -1,8 +1,8 @@
 package com.devstromo.easy.p112;
 
-public class Solution {
+class Solution {
 
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
         if (root.left == null && root.right == null) return targetSum == root.val;
 
