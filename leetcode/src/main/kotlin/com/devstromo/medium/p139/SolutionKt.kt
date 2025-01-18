b3d7c56d1136f8b6865b4041b1763385b406d789
alemoles <alemolerio@gmail.com>
2025-01-18 15:55:50 -0300

    Leetcode | Word Break | Kotlin

--- leetcode/src/main/kotlin/com/devstromo/medium/p139/SolutionKt.kt
+++ leetcode/src/main/kotlin/com/devstromo/medium/p139/SolutionKt.kt
@@ -18,4 +18,44 @@ class SolutionKt {
 
         return dp[n]
     }
+
+    fun wordBreakBest(s: String, wordDict: List<String>): Boolean {
+        class Node {
+            val next = Array<Node?>(26) {null}
+            var isEnd = false
+        }
+        val root = Node()
+        fun insert(str: String) {
+            var cur = root
+            for (c in str) {
+                val idx = c.code - 'a'.code
+                cur = cur.next[idx] ?: Node().apply { cur.next[idx] = this }
+            }
+            cur.isEnd = true
+        }
+
+        fun dpSearch(str: String): Boolean {
+            val dp = BooleanArray(str.length+1) {false}
+            dp[0] = true
+            for (i in str.indices) {
+                if (!dp[i]) continue
+                var cur = root
+                for (j in i until str.length) {
+                    val idx = str[j].code - 'a'.code
+
+                    cur = cur.next[idx] ?: break
+
+                    if (cur.isEnd)
+                        dp[j+1] = true
+                }
+                //if (dp[str.length]) return true
+            }
+            return dp[str.length]
+        }
+
+        for (word in wordDict)
+            if (!dpSearch(word)) insert(word)
+
+        return dpSearch(s)
+    }
 }
--- leetcode/src/test/kotlin/com/devstromo/medium/p139/SolutionKtUnitTest.kt
+++ leetcode/src/test/kotlin/com/devstromo/medium/p139/SolutionKtUnitTest.kt
@@ -13,4 +13,11 @@ class SolutionKtUnitTest {
         assertTrue(solution.wordBreak("applepenapple", listOf("apple", "pen")))
         assertFalse(solution.wordBreak("catsandog", listOf("cats", "dog", "sand", "and", "cat")))
     }
+
+    @Test
+    fun `Test Word Break Best`() {
+        assertTrue(solution.wordBreakBest("leetcode", listOf("leet", "code")))
+        assertTrue(solution.wordBreakBest("applepenapple", listOf("apple", "pen")))
+        assertFalse(solution.wordBreakBest("catsandog", listOf("cats", "dog", "sand", "and", "cat")))
+    }
 }
