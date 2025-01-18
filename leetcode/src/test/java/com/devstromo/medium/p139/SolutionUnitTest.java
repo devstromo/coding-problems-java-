b99da7e8284ad3b0030666500fe194d432f8063a
alemoles <alemolerio@gmail.com>
2025-01-18 15:39:01 -0300

    Leetcode | Word Break | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p139/SolutionUnitTest.java
@@ -0,0 +1,21 @@
+package com.devstromo.medium.p139;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Word Break")
+    void testWordBreak() {
+        assertTrue(solution.wordBreak("leetcode", List.of("leet", "code")));
+        assertTrue(solution.wordBreak("applepenapple", List.of("apple", "pen")));
+        assertFalse(solution.wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")));
+    }
+
+}
