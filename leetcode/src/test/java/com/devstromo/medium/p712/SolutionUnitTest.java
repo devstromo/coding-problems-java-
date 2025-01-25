5b6b96521e6a1b7faf4e15d829a6ea1fd65591c1
alemoles <alemolerio@gmail.com>
2025-01-25 20:31:44 -0300

    Leetcode | Minimum ASCII Delete Sum for Two Strings | Java

---
+++ leetcode/src/test/java/com/devstromo/medium/p712/SolutionUnitTest.java
@@ -0,0 +1,17 @@
+package com.devstromo.medium.p712;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Minimum ASCII Delete Sum for Two Strings")
+    void testMinimumASCIIDeleteSumForTwoStrings() {
+        assertEquals(231, solution.minimumDeleteSum("sea", "eat"));
+        assertEquals(403, solution.minimumDeleteSum("delete", "leet"));
+    }
+}
