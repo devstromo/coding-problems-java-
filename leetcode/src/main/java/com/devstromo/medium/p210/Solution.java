0da3433fbdcbf472853befeed585c65a9614ec84
alemoles <alemolerio@gmail.com>
2025-01-25 19:39:06 -0300

    Leetcode | Course Schedule II | Java

---
+++ leetcode/src/main/java/com/devstromo/medium/p210/Solution.java
@@ -0,0 +1,46 @@
+package com.devstromo.medium.p210;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] prereq : prerequisites) {
+            graph.get(prereq[1]).add(prereq[0]);
+            inDegree[prereq[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int[] order = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            order[index++] = current;
+
+            for (int neighbor : graph.get(current)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        return index == numCourses ? order : new int[0];
+    }
+}
---
+++ leetcode/src/test/java/com/devstromo/medium/p210/SolutionUnitTest.java
@@ -0,0 +1,37 @@
+package com.devstromo.medium.p210;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Course Schedule II")
+    public void testCourseScheduleII() {
+        assertArrayEquals(
+                new int[]{0, 1},
+                solution.findOrder(2, new int[][]{
+                        {1, 0},
+                }));
+        int[] actual = solution.findOrder(4, new int[][]{
+                {1, 0},
+                {2, 0},
+                {3, 1},
+                {3, 2},
+        });
+        List<int[]> expectedOutputs = List.of(
+                new int[]{0, 1, 2, 3},
+                new int[]{0, 2, 1, 3}
+        );
+
+        assertTrue(
+                expectedOutputs.stream().anyMatch(expected -> Arrays.equals(expected, actual))
+        );
+    }
+}
