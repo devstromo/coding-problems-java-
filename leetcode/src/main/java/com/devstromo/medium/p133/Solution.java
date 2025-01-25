682c13efc979fa48d8f1e292f2ae50e7dcbd31db
alemoles <alemolerio@gmail.com>
2025-01-24 23:16:15 -0300

    Leetcode | Clone Graph | Java

---
+++ leetcode/src/main/java/com/devstromo/medium/p133/Solution.java
@@ -0,0 +1,51 @@
+package com.devstromo.medium.p133;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        Map<Node, Node> visited = new HashMap<>();
+
+        return dfs(node, visited);
+    }
+
+    private Node dfs(Node node, Map<Node, Node> visited) {
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node clone = new Node(node.val);
+        visited.put(node, clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor, visited));
+        }
+
+        return clone;
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
---
+++ leetcode/src/test/java/com/devstromo/medium/p133/SolutionUnitTest.java
@@ -0,0 +1,60 @@
+package com.devstromo.medium.p133;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionUnitTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("Test Clone Graph")
+    void testCloneGraph() {
+        Solution.Node node1 = new Solution.Node(1);
+        Solution.Node node2 = new Solution.Node(2);
+        Solution.Node node3 = new Solution.Node(3);
+        Solution.Node node4 = new Solution.Node(4);
+
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+
+        Solution.Node clonedGraph = solution.cloneGraph(node1);
+
+        assertNotNull(clonedGraph);
+
+        Map<Solution.Node, Solution.Node> visited = new HashMap<>();
+        assertTrue(areGraphsEqual(node1, clonedGraph, visited));
+
+        clonedGraph.neighbors.get(0).val = 99;
+        assertNotEquals(node1.neighbors.get(0).val, clonedGraph.neighbors.get(0).val);
+    }
+
+    private boolean areGraphsEqual(Solution.Node node1, Solution.Node node2, Map<Solution.Node, Solution.Node> visited) {
+        if (node1 == null || node2 == null) return node1 == node2;
+        if (visited.containsKey(node1)) return visited.get(node1) == node2;
+
+        if (node1.val != node2.val || node1.neighbors.size() != node2.neighbors.size()) {
+            return false;
+        }
+
+        visited.put(node1, node2);
+
+        for (int i = 0; i < node1.neighbors.size(); i++) {
+            if (!areGraphsEqual(node1.neighbors.get(i), node2.neighbors.get(i), visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
