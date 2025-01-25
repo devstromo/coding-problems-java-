bd49167ea32e7343054c3a79550dce0ae0749d91
alemoles <alemolerio@gmail.com>
2025-01-25 19:40:25 -0300

    Leetcode | Course Schedule II | Java

--- leetcode/src/main/java/com/devstromo/medium/p210/Solution.java
+++ leetcode/src/main/java/com/devstromo/medium/p210/Solution.java
@@ -43,4 +43,56 @@ public class Solution {
 
         return index == numCourses ? order : new int[0];
     }
+
+    private int top;
+
+    public int[] findOrderBest(int numCourses, int[][] prerequisites) {
+        top = numCourses - 1;
+
+        int[] postList = new int[numCourses];
+        int[] nextIndex = new int[prerequisites.length];
+        int[] nextNode = new int[prerequisites.length];
+
+        for (int i = 0; i < numCourses; i++) {
+            postList[i] = -1;
+        }
+
+        int index = 0;
+        for (int[] prerequisite : prerequisites) {
+            nextIndex[index] = postList[prerequisite[1]];
+            nextNode[index] = prerequisite[0];
+            postList[prerequisite[1]] = index;
+            index++;
+        }
+
+        int[] stack = new int[numCourses];
+        int[] visited = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (0 == visited[i]) {
+                if (!dfs(i, visited, postList, nextIndex, nextNode, stack)) {
+                    return new int[0];
+                }
+            }
+        }
+        return stack;
+    }
+
+    private boolean dfs(int numCourses, int[] visited, int[] postList, int[] nextIndex, int[] nextNode, int[] stack) {
+        visited[numCourses] = 1;
+        for (int i = postList[numCourses]; i != -1; i = nextIndex[i]) {
+            if (1 == visited[nextNode[i]]) {
+                return false;
+            }
+            if (0 == visited[nextNode[i]]) {
+                if (!dfs(nextNode[i], visited, postList, nextIndex, nextNode, stack)) {
+                    return false;
+                }
+            }
+        }
+        visited[numCourses] = 2;
+        stack[top] = numCourses;
+        top--;
+        return true;
+    }
 }
--- leetcode/src/test/java/com/devstromo/medium/p210/SolutionUnitTest.java
+++ leetcode/src/test/java/com/devstromo/medium/p210/SolutionUnitTest.java
@@ -34,4 +34,23 @@ class SolutionUnitTest {
                 expectedOutputs.stream().anyMatch(expected -> Arrays.equals(expected, actual))
         );
     }
+
+    @Test
+    @DisplayName("Test Course Schedule II Best")
+    public void testCourseScheduleIIBest() {
+        int[] actual = solution.findOrderBest(4, new int[][]{
+                {1, 0},
+                {2, 0},
+                {3, 1},
+                {3, 2},
+        });
+        List<int[]> expectedOutputs = List.of(
+                new int[]{0, 1, 2, 3},
+                new int[]{0, 2, 1, 3}
+        );
+
+        assertTrue(
+                expectedOutputs.stream().anyMatch(expected -> Arrays.equals(expected, actual))
+        );
+    }
 }
