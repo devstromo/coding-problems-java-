617c6b9ddadee2032d25d5bf1a735976ff4975fb
alemoles <alemolerio@gmail.com>
2025-01-28 21:45:42 -0300

    Leetcode | Longest Univalue Path | Kotlin

--- leetcode/src/main/kotlin/com/devstromo/medium/p687/SolutionKt.kt
+++ leetcode/src/main/kotlin/com/devstromo/medium/p687/SolutionKt.kt
@@ -30,6 +30,32 @@ class SolutionKt {
         return max(leftPath, rightPath)
     }
 
+    fun longestUnivaluePathBest(root: TreeNode?): Int {
+        var longestPath = 0
+
+        fun longestPath(node: TreeNode?, previousVal: Int? = null): Int {
+            if (node == null) return 0
+
+            val repeatedLeftLength = longestPath(node.left, node.`val`)
+            val repeatedRightLength = longestPath(node.right, node.`val`)
+
+            longestPath = maxOf(longestPath, repeatedLeftLength + repeatedRightLength)
+
+            var length = maxOf(repeatedLeftLength, repeatedRightLength)
+            if (node.`val` == previousVal) {
+                length++
+            } else {
+                return 0
+            }
+
+            return length
+        }
+
+        longestPath(root)
+
+        return longestPath
+    }
+
     class TreeNode(var `val`: Int) {
         var left: TreeNode? = null
         var right: TreeNode? = null
--- leetcode/src/test/kotlin/com/devstromo/medium/p687/SolutionKtUnitTest.kt
+++ leetcode/src/test/kotlin/com/devstromo/medium/p687/SolutionKtUnitTest.kt
@@ -1,15 +1,13 @@
 package com.devstromo.medium.p687
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class SolutionKtUnitTest {
     private val solution = SolutionKt()
 
     @Test
-    @DisplayName("Test Longest Univalue Path")
-    fun testLongestUnivaluePath() {
+    fun `Test Longest Univalue Path`() {
         val root = SolutionKt.TreeNode(5).apply {
             left = SolutionKt.TreeNode(4).apply {
                 left = SolutionKt.TreeNode(1)
@@ -22,4 +20,17 @@ class SolutionKtUnitTest {
         assertEquals(2, solution.longestUnivaluePath(root))
     }
 
+    @Test
+    fun `Test Longest Univalue Path Best`() {
+        val root = SolutionKt.TreeNode(5).apply {
+            left = SolutionKt.TreeNode(4).apply {
+                left = SolutionKt.TreeNode(1)
+                right = SolutionKt.TreeNode(1)
+            }
+            right = SolutionKt.TreeNode(5).apply {
+                right = SolutionKt.TreeNode(5)
+            }
+        }
+        assertEquals(2, solution.longestUnivaluePath(root))
+    }
 }
