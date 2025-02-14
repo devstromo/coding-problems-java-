32b53e5804d925e79bf6a5c0a6f327c456f2a088
alemoles <alemolerio@gmail.com>
2025-02-13 22:04:52 -0300

    Leetcode | Binary Tree Zigzag Level Order Traversal | Kotlin

--- leetcode/src/main/kotlin/com/devstromo/medium/p103/SolutionKt.kt
+++ leetcode/src/main/kotlin/com/devstromo/medium/p103/SolutionKt.kt
@@ -1,5 +1,8 @@
 package com.devstromo.medium.p103
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class SolutionKt {
     fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
@@ -29,6 +32,29 @@ class SolutionKt {
             flag = !flag
         }
         return result
+    }
+
+    fun zigzagLevelOrderBest(root: TreeNode?): List<List<Int>> {
+        val r = ArrayList<LinkedList<Int>>()
+
+        fun TreeNode.dfs(depth: Int = 0) {
+            if (depth == r.size) {
+                r += LinkedList<Int>()
+            }
+
+            if (depth % 2 == 0) {
+                r[depth].offer(`val`)
+            } else {
+                r[depth].push(`val`)
+            }
+
+            left?.dfs(depth + 1)
+            right?.dfs(depth + 1)
+        }
+
+        root?.dfs()
+
+        return r
     }
 
     class TreeNode(var `val`: Int) {
--- leetcode/src/test/kotlin/com/devstromo/medium/p103/SolutionKtUnitTest.kt
+++ leetcode/src/test/kotlin/com/devstromo/medium/p103/SolutionKtUnitTest.kt
@@ -21,4 +21,20 @@ class SolutionKtUnitTest {
         assertEquals(listOf<SolutionKt.TreeNode>(), solution.zigzagLevelOrder(null))
 
     }
+
+    @Test
+    fun `Binary Tree Zigzag Level Order Traversal Best`() {
+        val root = SolutionKt.TreeNode(3).apply {
+            left = SolutionKt.TreeNode(9)
+            right = SolutionKt.TreeNode(20).apply {
+                left = SolutionKt.TreeNode(15)
+                right = SolutionKt.TreeNode(7)
+            }
+        }
+        assertEquals(listOf(listOf(3), listOf(20, 9), listOf(15, 7)), solution.zigzagLevelOrderBest(root))
+        val singleRoot = SolutionKt.TreeNode(1)
+        assertEquals(listOf(listOf(1)), solution.zigzagLevelOrderBest(singleRoot))
+        assertEquals(listOf<SolutionKt.TreeNode>(), solution.zigzagLevelOrderBest(null))
+
+    }
 }
