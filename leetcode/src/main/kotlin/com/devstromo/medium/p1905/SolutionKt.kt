2c93eb643f49855dc5f81e23c16a1a9907bec3c0
alemoles <alemolerio@gmail.com>
2025-03-09 16:16:20 -0300

    Leetcode | Count Sub Islands | Kotlin

---
+++ leetcode/src/main/kotlin/com/devstromo/medium/p1905/SolutionKt.kt
@@ -0,0 +1,44 @@
+package com.devstromo.medium.p1905
+
+class SolutionKt {
+    private var rows = 0
+    private var cols: Int = 0
+    private val directions: Array<IntArray> = arrayOf(intArrayOf(-1, 0), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(0, 1))
+    fun countSubIslands(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
+        fun dfs(grid: Array<IntArray>, r: Int, c: Int) {
+            if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == 0) {
+                return
+            }
+            grid[r][c] = 0 // Mark as visited
+            for (dir in directions) {
+                dfs(grid, r + dir[0], c + dir[1])
+            }
+        }
+
+        rows = grid1.size
+        cols = grid1[0].size
+
+
+        // Step 1: Remove all invalid islands in grid2
+        for (r in 0..<rows) {
+            for (c in 0..<cols) {
+                if (grid2[r][c] == 1 && grid1[r][c] == 0) {
+                    dfs(grid2, r, c) // Remove invalid land
+                }
+            }
+        }
+
+
+        // Step 2: Count valid sub-islands
+        var subIslands = 0
+        for (r in 0..<rows) {
+            for (c in 0..<cols) {
+                if (grid2[r][c] == 1) {
+                    subIslands++ // Found a valid sub-island
+                    dfs(grid2, r, c) // Mark the entire island as visited
+                }
+            }
+        }
+        return subIslands
+    }
+}
---
+++ leetcode/src/test/kotlin/com/devstromo/medium/p1905/SolutionKtUnitTest.kt
@@ -0,0 +1,48 @@
+package com.devstromo.medium.p1905
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionKtUnitTest {
+    private val solution = SolutionKt()
+
+    @Test
+    fun `Test Count Sub Islands`() {
+        assertEquals(
+            3, solution.countSubIslands(
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 0, 1, 1)
+                ),
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 0, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 0, 0),
+                    intArrayOf(1, 0, 1, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0)
+                )
+            )
+        )
+        assertEquals(
+            2, solution.countSubIslands(
+                arrayOf(
+                    intArrayOf(1, 0, 1, 0, 1),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(1, 0, 1, 0, 1)
+                ),
+                arrayOf(
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(1, 0, 0, 0, 1)
+                )
+            )
+        )
+    }
+}
