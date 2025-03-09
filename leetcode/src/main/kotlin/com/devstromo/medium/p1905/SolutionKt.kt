7e75bfd606672037a086d6252876de50804905ce
alemoles <alemolerio@gmail.com>
2025-03-09 16:17:50 -0300

    Leetcode | Count Sub Islands | Kotlin

--- leetcode/src/main/kotlin/com/devstromo/medium/p1905/SolutionKt.kt
+++ leetcode/src/main/kotlin/com/devstromo/medium/p1905/SolutionKt.kt
@@ -3,7 +3,9 @@ package com.devstromo.medium.p1905
 class SolutionKt {
     private var rows = 0
     private var cols: Int = 0
-    private val directions: Array<IntArray> = arrayOf(intArrayOf(-1, 0), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(0, 1))
+    private val directions: Array<IntArray> =
+        arrayOf(intArrayOf(-1, 0), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(0, 1))
+
     fun countSubIslands(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
         fun dfs(grid: Array<IntArray>, r: Int, c: Int) {
             if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == 0) {
@@ -40,5 +42,30 @@ class SolutionKt {
             }
         }
         return subIslands
+    }
+
+    fun countSubIslandsBest(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
+        var count = 0
+        for (row in grid2.indices) {
+            for (col in grid2[0].indices) {
+                if (grid2[row][col] == 1 && dfs(grid1, grid2, row, col)) {
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
+    private fun dfs(grid1: Array<IntArray>, grid2: Array<IntArray>, row: Int, col: Int): Boolean {
+        val m = grid1.size
+        val n = grid1[0].size
+        if (row !in 0..(m - 1) || col !in 0..(n - 1) || grid2[row][col] == 0) return true
+        grid2[row][col] = 0
+        var result = grid1[row][col] == 1
+        result = dfs(grid1, grid2, row + 1, col) && result
+        result = dfs(grid1, grid2, row - 1, col) && result
+        result = dfs(grid1, grid2, row, col + 1) && result
+        result = dfs(grid1, grid2, row, col - 1) && result
+        return result
     }
 }
--- leetcode/src/test/kotlin/com/devstromo/medium/p1905/SolutionKtUnitTest.kt
+++ leetcode/src/test/kotlin/com/devstromo/medium/p1905/SolutionKtUnitTest.kt
@@ -45,4 +45,44 @@ class SolutionKtUnitTest {
             )
         )
     }
+
+    @Test
+    fun `Test Count Sub Islands Best`() {
+        assertEquals(
+            3, solution.countSubIslandsBest(
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 0, 1, 1)
+                ),
+                arrayOf(
+                    intArrayOf(1, 1, 1, 0, 0),
+                    intArrayOf(0, 0, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 0, 0),
+                    intArrayOf(1, 0, 1, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0)
+                )
+            )
+        )
+        assertEquals(
+            2, solution.countSubIslandsBest(
+                arrayOf(
+                    intArrayOf(1, 0, 1, 0, 1),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(1, 0, 1, 0, 1)
+                ),
+                arrayOf(
+                    intArrayOf(0, 0, 0, 0, 0),
+                    intArrayOf(1, 1, 1, 1, 1),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(0, 1, 0, 1, 0),
+                    intArrayOf(1, 0, 0, 0, 1)
+                )
+            )
+        )
+    }
 }
